bbce6f8b4ae1e05c2390e3e19c81de6b90d2e7e7
Mairajuddin <qazimairaj4@gmail.com>
2024-02-07 14:39:06 +0500

    complete selectProduct Page

--- src/Components/SelectProductsToCart/SelectProduct.jsx
+++ src/Components/SelectProductsToCart/SelectProduct.jsx
@@ -1,12 +1,66 @@
-import { Box, Container, Divider, Grid, Typography } from "@mui/material";
 import { useState } from "react";
+import CardComp from "../LandingPage/CardComp";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const SelectProduct = () => {
-  const [color, setColor] = useState([]);
+  const [color, setColor] = useState(null);
+  const [size, setSize] = useState(null);
+  const [count, setCount] = useState(0);
+  const colors = ["#D0EC48", "#565656"];
 
-  const handleSetColor = (e) => {
-    console.log(e, "indexxxxxxxxxxxx");
+  const productSize = [30, 56, 42, 48];
+
+  const handleSetColor = (data) => {
+    setColor(data);
+    console.log(data);
   };
+  const handleSize = (data) => {
+    setSize(data);
+  };
+  const handleCountDecrese = () => {
+    if (count !== 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const RelatedCard = [
+    {
+      id: "1",
+      img: "./camera 1.png",
+      title: "Camera",
+      price: "31, 70",
+    },
+    {
+      id: "2",
+      img: "./Frame 29.png",
+      title: "Headphones",
+      price: "21, 70",
+    },
+    {
+      id: "3",
+      img: "./Frame 30.png",
+      title: "Laptop",
+      price: "11, 70",
+    },
+    {
+      id: "4",
+      img: "./Frame 31.png",
+      title: "Gaming Headphone",
+      price: "21, 70",
+    },
+  ];
+
   return (
     <Container>
       <Box my={2}>
@@ -56,7 +110,7 @@ const SelectProduct = () => {
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                gap: 2,
+                gap: 0.8,
                 width: "100%",
                 // border: "1px solid green",
               }}
@@ -130,24 +184,299 @@ const SelectProduct = () => {
               >
                 Hurry up ! only 34 product left in stock !
               </Typography>
-              <Divider sx={{ my: 2 }} variant="middle" />
-              <Box display="flex" gap={2}>
-                {[1, 2].map((_, index) => (
+              <Divider sx={{ my: 1 }} variant="middle" />
+              <Box display="flex" gap={2} sx={{ alignItems: "center" }}>
+                <span
+                  style={{
+                    fontSize: "18px",
+                    fontFamily: "sans-serif",
+                    color: "#4A4A4A",
+                    fontWeight: 700,
+                  }}
+                >
+                  Color {"   "}:
+                </span>
+                {colors.map((item, index) => (
                   <Box
                     key={index}
-                    onClick={() => handleSetColor(index)}
                     sx={{
-                      height: "20px",
-                      width: "20px",
+                      border: index === color ? "2px solid red" : "none",
                       borderRadius: "50%",
-                      backgroundColor: "#D0EC48",
+                      cursor: "pointer",
+                      p: 0.3,
+                    }}
+                  >
+                    <Box
+                      onClick={() => handleSetColor(index)}
+                      sx={{
+                        height: "20px",
+                        width: "20px",
+                        borderRadius: "50%",
+                        // border: index === color ? "1.3px solid red" : "none",
+                        p: 1,
+                        backgroundColor: item,
+                        boxSizing: "border-box",
+                      }}
+                    />
+                  </Box>
+                ))}
+              </Box>
+              <Box mt={1} display="flex" gap={2} sx={{ alignItems: "center" }}>
+                <span
+                  style={{
+                    fontSize: "18px",
+                    fontFamily: "sans-serif",
+                    color: "#4A4A4A",
+                    fontWeight: 700,
+                  }}
+                >
+                  Size {"    "}:
+                </span>
+                {productSize.map((item, index) => (
+                  <Box
+                    key={"index"}
+                    sx={{
+                      backgroundColor: "#EEEEEE",
+                      p: 1.5,
+                      border: size === index ? "1px solid black" : null,
+                      cursor: "pointer",
+                    }}
+                    onClick={() => handleSize(index)}
+                  >
+                    <span
+                      style={{ fontSize: "20px", fontFamily: "sans-serif" }}
+                    >
+                      {item}
+                    </span>
+                  </Box>
+                ))}
+              </Box>
+              <Box
+                mt={1.9}
+                display="flex"
+                gap={2}
+                sx={{ alignItems: "center" }}
+              >
+                <span
+                  style={{
+                    fontSize: "18px",
+                    fontFamily: "sans-serif",
+                    fontWeight: 700,
+                    color: "#4A4A4A",
+                  }}
+                >
+                  Quantity{"  "}:
+                </span>
+                <Box
+                  sx={{
+                    display: "flex",
+                    backgroundColor: "#EEEEEE",
+
+                    fontSize: "30px",
+                    alignItems: "center",
+                    fontWeight: 700,
+                    justifyContent: "center",
+                  }}
+                >
+                  <span
+                    onClick={handleCountDecrese}
+                    style={{
+                      fontSize: "20px",
+                      padding: "8px",
+                      border: "1px solid black",
+                      cursor: "pointer",
+                      fontWeight: "bolder !important",
+                    }}
+                  >
+                    -
+                  </span>
+                  <span
+                    style={{
+                      fontSize: "20px",
+                      padding: "8px",
+                      paddingInline: "40px",
+                      border: "1px solid black",
+                      cursor: "pointer",
+                    }}
+                  >
+                    {count}
+                  </span>
+                  <span
+                    onClick={() => setCount(count + 1)}
+                    style={{
+                      cursor: "pointer",
+                      fontSize: "20px",
+                      padding: "8px",
+                      border: "1px solid black",
+                    }}
+                  >
+                    +
+                  </span>
+                </Box>
+              </Box>
+              <Box
+                mt={2}
+                display="flex"
+                gap={2}
+                // p={1}
+                sx={{ flexDirection: { xs: "column", sm: "row" } }}
+              >
+                <Button
+                  variant="contained"
+                  size="large"
+                  sx={{
+                    px: 5,
+                    py: 1.5,
+                    borderRadius: "30px",
+                    fontWeight: 800,
+                    backgroundColor: "#EDA415",
+                  }}
+                >
+                  <span>Add to Cart</span>
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{
+                    px: 5,
+                    py: 1.5,
+                    fontWeight: 800,
+                    borderRadius: "30px",
+                    backgroundColor: "#EDA415",
+                  }}
+                  size="large"
+                >
+                  {" "}
+                  <span>Buy it Now</span>
+                </Button>
+                <Box
+                  sx={{
+                    display: { xs: "none", md: "block" },
+                    p: 2,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: "50%",
+                    backgroundColor: "#EEEEEE",
+                  }}
+                >
+                  <img
+                    src="./VectorHeart.png"
+                    alt="hert"
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
                     }}
                   />
-                ))}
+                </Box>
+              </Box>
+              <Divider sx={{ my: 2 }} variant="middle" />
+              <Box display="flex" gap={2} sx={{ flexDirection: "column" }}>
+                <Box display="flex" gap={2} sx={{ alignItems: "center" }}>
+                  <span>SKU</span>
+                  <Typography>8-89898098-99</Typography>
+                </Box>
+                <Box display="flex" gap={2}>
+                  <span>Category</span>
+                  <Box display={"flex"} gap={2}>
+                    <Typography>20%{"  "}off</Typography>
+                    <Typography>49%{"  "}off</Typography>
+                    <Typography>Alex Remote</Typography>
+                  </Box>
+                </Box>
+                <Box display="flex" gap={2} sx={{ alignItems: "center" }}>
+                  <span>Share</span>
+                  <Box display={"flex"} gap={4}>
+                    <FacebookIcon
+                      sx={{ color: "#232323", height: "25px", width: "25px" }}
+                    />
+                    <GoogleIcon
+                      sx={{ color: "#232323", height: "25px", width: "25px" }}
+                    />
+                    <InstagramIcon
+                      sx={{ color: "#232323", height: "25px", width: "25px" }}
+                    />
+                  </Box>
+                </Box>
               </Box>
             </Box>
           </Grid>
         </Grid>
+        <Box
+          width="100%"
+          mt={3}
+          sx={{
+            gap: 4,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            size="large"
+            sx={{
+              // border: "1px solid black",
+              borderRadius: "15px",
+              px: 5,
+              textTransform: "none",
+            }}
+          >
+            Description
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{ borderRadius: "15px", px: 5, textTransform: "none" }}
+          >
+            Reviews
+          </Button>
+        </Box>
+        <Box
+          width="100%"
+          p={5}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            mt: 3,
+            border: "1px solid #B8B8B8",
+            borderRadius: "15px",
+            gap: 1,
+          }}
+        >
+          <Typography
+            variant="body1"
+            sx={{ color: "#003F62", fontWeight: 700, pb: 1 }}
+          >
+            Customer Reviews
+          </Typography>
+          <Typography>Not reviews yet</Typography>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ width: "180px", borderRadius: 0 }}
+          >
+            write a review
+          </Button>
+        </Box>
+        <Box mt={5}>
+          <Typography variant="h5" sx={{ color: "#003F62", fontWeight: 700 }}>
+            Related Products
+          </Typography>
+          <Box
+            display="flex"
+            mt={3}
+            sx={{
+              flexWrap: "wrap",
+              gap: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CardComp data={RelatedCard} />
+          </Box>
+        </Box>
       </Box>
     </Container>
   );
